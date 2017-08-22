da81c5aa0d696df1df3ad9943678180e85c58df1
manniR <manni@manfredraggl.com>
2017-08-22 17:19:52 +0200

    initial commit

---
+++ craft/config/db.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Database Configuration
+ */
+
+return array(
+  // All environments
+  '*' => array(
+    'tablePrefix' => '',
+    'server' => '',
+    'database' => '',
+    'user' => '',
+    'password' => '',
+  ),
+
+  // Live (production) environment
+  'productive'  => array(
+  ),
+
+  // Staging (pre-production) environment
+  'staging'  => array(
+  ),
+
+  // Local (development) environment
+  'local'  => array(
+  ),
+);
